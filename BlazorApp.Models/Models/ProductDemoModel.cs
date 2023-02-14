fa330e3698de35dd2a0f6e0feb059ec62220f7da
iretibe <yessouf2009@yahoo.fr>
2023-02-14 20:01:16 +0000

    Started Blazor App

---
+++ BlazorApp.Models/Models/ProductDemoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Models.Models
+{
+    public class ProductDemoModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<ProductProDemoModel> ProductProperties { get; set; }
+    }
+}
---
+++ BlazorApp.Models/Models/ProductProDemoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Models.Models
+{
+    public class ProductProDemoModel
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
